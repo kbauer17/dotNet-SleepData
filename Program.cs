5438bcfad94a58c420dfdae797e835b80f991fc1
Kathy Bauer <kbauer17@my.wctc.edu>
2023-02-06 13:49:47 -0600

    [1.04] determine end date

--- Program.cs
+++ Program.cs
@@ -14,6 +14,13 @@ if (resp == "1")
     Console.WriteLine("How many weeks of data is needed?");
     // input the response (convert to int)
     int weeks = int.Parse(Console.ReadLine());
+        // determine start and end date
+    DateTime today = DateTime.Now;
+    // we want full weeks sunday - saturday
+    DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+    // subtract # of weeks from endDate to get startDate
+    DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+    Console.WriteLine(dataDate);
 }
 else if (resp == "2")
 {
