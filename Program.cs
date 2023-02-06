2887fcf32bd03016d430c95619e6f662ad260ebd
Kathy Bauer <kbauer17@my.wctc.edu>
2023-02-06 13:53:52 -0600

    [1.05] Generate sleep data - Loop for weeks of data - For each week, generate 7 days of random sleep data - Output data to console

--- Program.cs
+++ Program.cs
@@ -20,7 +20,24 @@ if (resp == "1")
     DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
     // subtract # of weeks from endDate to get startDate
     DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
-    Console.WriteLine(dataDate);
+        // random number generator
+    Random rnd = new Random();
+
+    // loop for the desired # of weeks
+    while (dataDate < dataEndDate)
+    {
+        // 7 days in a week
+        int[] hours = new int[7];
+        for (int i = 0; i < hours.Length; i++)
+        {
+            // generate random number of hours slept between 4-12 (inclusive)
+            hours[i] = rnd.Next(4, 13);
+        }
+        // M/d/yyyy,#|#|#|#|#|#|#
+        Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+        // add 1 week to date
+        dataDate = dataDate.AddDays(7);
+    }
 }
 else if (resp == "2")
 {
