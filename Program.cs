9b878224c85d13763dad96a38bddd744f20e1890
Kathy Bauer <kbauer17@my.wctc.edu>
2023-02-06 16:01:37 -0600

    [1.09] Final comments and push

--- Program.cs
+++ Program.cs
@@ -14,13 +14,13 @@ if (resp == "1")
     Console.WriteLine("How many weeks of data is needed?");
     // input the response (convert to int)
     int weeks = int.Parse(Console.ReadLine());
-        // determine start and end date
+    // determine start and end date
     DateTime today = DateTime.Now;
     // we want full weeks sunday - saturday
     DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
     // subtract # of weeks from endDate to get startDate
     DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
-        // random number generator
+    // random number generator
     Random rnd = new Random();
      // create file
     StreamWriter sw = new StreamWriter("data.txt");
@@ -49,21 +49,22 @@ else if (resp == "2")
 {
     // parse data file
 
-    // Read and show each line from the file.
-            string line = "";
+    
+            //string line = "";
             using (StreamReader sr = new StreamReader("data.txt"))
             {
-                while ((line = sr.ReadLine()) != null)
+                while (!sr.EndOfStream)
                 {
-                    //Console.WriteLine(line);
+                    string line = sr.ReadLine();
+                    // parse the date from each line of data and output to console with headers
                     string date = line.Substring(0,line.IndexOf(','));
                     var parsedDate = DateTime.Parse(date);
-                    Console.WriteLine($"Week of {parsedDate:MMMM, dd, yyyy}:");
+                    Console.WriteLine($"Week of {parsedDate:MMM, dd, yyyy}:");
                     Console.WriteLine("{0,3}{1,3}{2,3}{3,3}{4,3}{5,3}{6,3}","Su","Mo","Tu","We","Th","Fr","Sa");
                     Console.WriteLine("{0,3}{1,3}{2,3}{3,3}{4,3}{5,3}{6,3}","--","--","--","--","--","--","--");
 
+                    // parse the hour data from each line of date and output to console aligned to header/columns
                     string hours = line.Substring(line.IndexOf(',')+1);
-                    //System.Console.WriteLine(hours);
                     String[] arr = hours.Split('|');
                     Console.WriteLine("{0,3}{1,3}{2,3}{3,3}{4,3}{5,3}{6,3}",arr[0],arr[1],arr[2],arr[3],arr[4],arr[5],arr[6]);
                     System.Console.WriteLine();
