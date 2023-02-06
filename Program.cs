1524bc1117e354dd9773568b24883628d7bae06d
Kathy Bauer <kbauer17@my.wctc.edu>
2023-02-06 13:43:15 -0600

    [1.02] added user menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// ask for input
+Console.WriteLine("Enter 1 to create data file.");
+Console.WriteLine("Enter 2 to parse data.");
+Console.WriteLine("Enter anything else to quit.");
+// input response
+string? resp = Console.ReadLine();
+
+if (resp == "1")
+{
+    // TODO: create data file
+}
+else if (resp == "2")
+{
+    // TODO: parse data file
+
+}
